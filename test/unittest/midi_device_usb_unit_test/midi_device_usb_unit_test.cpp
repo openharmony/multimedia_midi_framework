c056250842dbc317020304a55997ae997ad60306
Robin <chenhongjian3@h-partners.com>
2026-01-12 17:18:21 +0800

    add ut
    Signed-off-by: Robin <chenhongjian3@h-partners.com>

---
+++ test/unittest/midi_device_usb_unit_test/midi_device_usb_unit_test.cpp
@@ -0,0 +1,264 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "midi_device_driver.h"
+#include "midi_device_usb.h"
+#include "midi_info.h"
+#include "native_midi_base.h"
+#include "v1_0/imidi_interface.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using namespace OHOS;
+using namespace MIDI;
+using namespace testing;
+using namespace testing::ext;
+
+
+class MockIMidiInterface : public HDI::Midi::V1_0::IMidiInterface {
+public:
+    MOCK_METHOD(int32_t, GetDeviceList, (std::vector<HDI::Midi::V1_0::MidiDeviceInfo>& deviceList), (override));
+    MOCK_METHOD(int32_t, OpenDevice, (int64_t deviceId), (override));
+    MOCK_METHOD(int32_t, CloseDevice, (int64_t deviceId), (override));
+    MOCK_METHOD(int32_t, OpenInputPort, (int64_t deviceId, uint32_t portId, const sptr<HDI::Midi::V1_0::IMidiCallback>& dataCallback), (override));
+    MOCK_METHOD(int32_t, OpenOutputPort, (int64_t deviceId, uint32_t portId), (override));
+    MOCK_METHOD(int32_t, CloseInputPort, (int64_t deviceId, uint32_t portId), (override));
+    MOCK_METHOD(int32_t, CloseOutputPort, (int64_t deviceId, uint32_t portId), (override));
+    MOCK_METHOD(int32_t, SendMidiMessages, (int64_t deviceId, uint32_t portId, const std::vector<HDI::Midi::V1_0::MidiMessage>& messages), (override));
+};
+
+
+class MidiDeviceUsbUnitTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+
+    static void TearDownTestCase() {}
+
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
+
+
+/**
+ * @tc.name: GetRegisteredDevices_001
+ * @tc.desc: GetDeviceList returns empty -> GetRegisteredDevices should return empty.
+ * @tc.type: FUNC
+ */
+HWTEST_F(MidiDeviceUsbUnitTest, GetRegisteredDevices_001, TestSize.Level0)
+{
+    sptr<MockIMidiInterface> mockMidiHdi = sptr<MockIMidiInterface>::MakeSptr();
+    ASSERT_NE(nullptr, mockMidiHdi);
+
+    UsbMidiTransportDeviceDriver driver;
+    driver.midiHdi_ = mockMidiHdi;
+
+    EXPECT_CALL(*mockMidiHdi, GetDeviceList(_))
+        .Times(1)
+        .WillOnce(Invoke([](std::vector<HDI::Midi::V1_0::MidiDeviceInfo>& deviceList) {
+            deviceList.clear();
+            return MIDI_STATUS_OK;
+        }));
+
+    auto deviceInfos = driver.GetRegisteredDevices();
+    EXPECT_TRUE(deviceInfos.empty());
+}
+
+/**
+ * @tc.name: GetRegisteredDevices_002
+ * @tc.desc: Single device with multiple ports -> verify field mapping + ConvertToDeviceInformation loop.
+ * @tc.type: FUNC
+ */
+HWTEST_F(MidiDeviceUsbUnitTest, GetRegisteredDevices_002, TestSize.Level0)
+{
+    sptr<MockIMidiInterface> mockMidiHdi = sptr<MockIMidiInterface>::MakeSptr();
+    ASSERT_NE(nullptr, mockMidiHdi);
+
+    UsbMidiTransportDeviceDriver driver;
+    driver.midiHdi_ = mockMidiHdi;
+
+    int64_t expectedDeviceId = 12345;
+    int32_t expectedProtocol = 1;
+    uint32_t expectedPortId0 = 10;
+    uint32_t expectedPortId1 = 11;
+    int32_t expectedDirection0 = 0;
+    int32_t expectedDirection1 = 1;
+
+    EXPECT_CALL(*mockMidiHdi, GetDeviceList(_))
+        .Times(1)
+        .WillOnce(Invoke([&](std::vector<HDI::Midi::V1_0::MidiDeviceInfo>& deviceList) {
+            deviceList.clear();
+
+            HDI::Midi::V1_0::MidiDeviceInfo device {};
+            device.deviceId = expectedDeviceId;
+            device.protocol = HDI::Midi::V1_0::MIDI_PROTOCOL_1_0;
+            device.productName = "TestProduct";
+            device.vendorName = "TestVendor";
+
+            HDI::Midi::V1_0::MidiPortInfo port0 {};
+            port0.portId = expectedPortId0;
+            port0.name = "InputPort0";
+            port0.direction = HDI::Midi::V1_0::PORT_DIRECTION_INPUT;
+
+            HDI::Midi::V1_0::MidiPortInfo port1 {};
+            port1.portId = expectedPortId1;
+            port1.name = "OutputPort1";
+            port1.direction = HDI::Midi::V1_0::PORT_DIRECTION_OUTPUT;
+
+            device.ports.push_back(port0);
+            device.ports.push_back(port1);
+
+            deviceList.push_back(device);
+            return MIDI_STATUS_OK;
+        }));
+
+    auto deviceInfos = driver.GetRegisteredDevices();
+
+    ASSERT_EQ(1u, deviceInfos.size());
+    const auto& devInfo = deviceInfos[0];
+
+
+    EXPECT_EQ(expectedDeviceId, devInfo.driverDeviceId);
+    EXPECT_EQ(DEVICE_TYPE_USB, devInfo.deviceType);
+    EXPECT_EQ(static_cast<TransportProtocol>(expectedProtocol), devInfo.transportProtocol);
+    EXPECT_EQ("TestProduct", devInfo.productName);
+    EXPECT_EQ("TestVendor", devInfo.vendorName);
+
+    ASSERT_EQ(2u, devInfo.portInfos.size());
+
+    EXPECT_EQ(expectedPortId0, devInfo.portInfos[0].portId);
+    EXPECT_EQ("InputPort0", devInfo.portInfos[0].name);
+    EXPECT_EQ(static_cast<PortDirection>(expectedDirection0), devInfo.portInfos[0].direction);
+    EXPECT_EQ(static_cast<TransportProtocol>(expectedProtocol), devInfo.portInfos[0].transportProtocol);
+
+    EXPECT_EQ(expectedPortId1, devInfo.portInfos[1].portId);
+    EXPECT_EQ("OutputPort1", devInfo.portInfos[1].name);
+    EXPECT_EQ(static_cast<PortDirection>(expectedDirection1), devInfo.portInfos[1].direction);
+    EXPECT_EQ(static_cast<TransportProtocol>(expectedProtocol), devInfo.portInfos[1].transportProtocol);
+}
+
+
+/**
+ * @tc.name: OpenDevice001
+ * @tc.desc: open correct deviceId, expect ok
+ * @tc.type: FUNC
+ */
+HWTEST_F(MidiDeviceUsbUnitTest, OpenDevice001, TestSize.Level0)
+{
+    sptr<MockIMidiInterface> mockMidiHdi = sptr<MockIMidiInterface>::MakeSptr();
+    EXPECT_CALL(*mockMidiHdi, OpenDevice(123)).WillOnce(Return(0));
+
+    UsbMidiTransportDeviceDriver driver;
+    driver.midiHdi_ = mockMidiHdi;
+    EXPECT_EQ(0, driver.OpenDevice(123));
+}
+
+
+/**
+ * @tc.name: CloseDevice001
+ * @tc.desc: close correct deviceId, expect ok
+ * @tc.type: FUNC
+ */
+HWTEST_F(MidiDeviceUsbUnitTest, CloseDevice001, TestSize.Level0)
+{
+    sptr<MockIMidiInterface> mockMidiHdi = sptr<MockIMidiInterface>::MakeSptr();
+
+    EXPECT_CALL(*mockMidiHdi, CloseDevice(123)).WillOnce(Return(0));
+
+    UsbMidiTransportDeviceDriver driver;
+    driver.midiHdi_ = mockMidiHdi;
+    EXPECT_EQ(0, driver.CloseDevice(123));
+}
+
+
+/**
+ * @tc.name: OpenInputPort001
+ * @tc.desc: open correct port, expect ok
+ * @tc.type: FUNC
+ */
+HWTEST_F(MidiDeviceUsbUnitTest, OpenInputPort001, TestSize.Level0)
+{
+    constexpr int64_t deviceId = 100;
+    constexpr uint32_t portIndex = 1;
+    sptr<MockIMidiInterface> mockMidiHdi = sptr<MockIMidiInterface>::MakeSptr();
+    ASSERT_NE(nullptr, mockMidiHdi);
+    UsbMidiTransportDeviceDriver driver;
+    driver.midiHdi_ = mockMidiHdi;
+
+    bool callbackCalled = false;
+    std::vector<MidiEventInner> receivedEvents;
+    UmpInputCallback inputCallback = [&](std::vector<MidiEventInner> &events) {
+        callbackCalled = true;
+        receivedEvents = events;
+    };
+
+    sptr<HDI::Midi::V1_0::IMidiCallback> capturedCallback = nullptr;
+
+    EXPECT_CALL(*mockMidiHdi, OpenInputPort(deviceId, portIndex, _))
+        .Times(1)
+        .WillOnce(Invoke([&](int64_t, uint32_t, const sptr<HDI::Midi::V1_0::IMidiCallback>& dataCallback) {
+            capturedCallback = dataCallback;
+            return MIDI_STATUS_OK;
+        }));
+    
+    EXPECT_EQ(MIDI_STATUS_OK, driver.OpenInputPort(deviceId, portIndex, inputCallback));
+    ASSERT_NE(nullptr, capturedCallback);
+
+    std::vector<HDI::Midi::V1_0::MidiMessage> messages;
+    HDI::Midi::V1_0::MidiMessage message1;
+    message1.timestamp = 123;
+    message1.data = {0x11223344u, 0x55667788u};
+
+    HDI::Midi::V1_0::MidiMessage message2;
+    message2.timestamp = 456;
+    message2.data = {0xAABBCCDDu};
+
+    messages.push_back(message1);
+    messages.push_back(message2);
+
+    EXPECT_EQ(0, capturedCallback->OnMidiDataReceived(messages));
+    EXPECT_TRUE(callbackCalled);
+    ASSERT_EQ(2u, receivedEvents.size());
+    EXPECT_EQ(123u, receivedEvents[0].timestamp);
+    EXPECT_EQ(2u, receivedEvents[0].length);
+    ASSERT_NE(nullptr, receivedEvents[0].data);
+
+    EXPECT_EQ(456u, receivedEvents[1].timestamp);
+    EXPECT_EQ(1u, receivedEvents[1].length);
+    ASSERT_NE(nullptr, receivedEvents[1].data);
+}
+
+
+/**
+ * @tc.name: CloseInputPort001
+ * @tc.desc: close correct port, expect ok
+ * @tc.type: FUNC
+ */
+HWTEST_F(MidiDeviceUsbUnitTest, CloseInputPort001, TestSize.Level0)
+{
+    constexpr int64_t deviceId = 100;
+    constexpr uint32_t portIndex = 1;
+    MidiEventInner event;
+    sptr<MockIMidiInterface> mockMidiHdi = sptr<MockIMidiInterface>::MakeSptr();
+    ASSERT_NE(nullptr, mockMidiHdi);
+    UsbMidiTransportDeviceDriver driver;
+    driver.midiHdi_ = mockMidiHdi;
+
+    EXPECT_CALL(*mockMidiHdi, CloseInputPort(deviceId, portIndex)).WillOnce(Return(MIDI_STATUS_OK));
+
+    EXPECT_EQ(MIDI_STATUS_OK, driver.CloseInputPort(deviceId, portIndex));
+    EXPECT_EQ(0, driver.HanleUmpInput(deviceId, portIndex, event));
+}
---
+++ test/unittest/midi_server_unit_test/midi_server_unit_test.cpp
@@ -0,0 +1,286 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <map>
+#include "midi_info.h"
+#include "native_midi_base.h"
+#include "midi_client_in_server.h"
+#include "midi_listener_callback.h"
+#include "midi_server.h"
+#include "midi_test_common.h"
+#include "iservice_registry.h"
+#include "system_ability_definition.h"
+#include "message_parcel.h"
+#include "parcel.h"
+#include "iremote_stub.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using namespace OHOS;
+using namespace MIDI;
+using namespace testing;
+using namespace testing::ext;
+
+class MockIMidiCallback : public IMidiCallback {
+public:
+    MOCK_METHOD(int32_t, NotifyDeviceChange, (int32_t change, (const std::map<int32_t, std::string> &deviceInfo)), (override));
+    MOCK_METHOD(int32_t, NotifyError, (int32_t code), (override));
+    MOCK_METHOD(sptr<IRemoteObject>, AsObject, (), (override));
+};
+
+
+// class MockIRemoteObject : public IRemoteObject {
+// public:
+//     MockIRemoteObject() : IRemoteObject(u"IRemoteObject") {}
+//     MOCK_METHOD(int32_t, GetObjectRefCount, (), (override));
+//     MOCK_METHOD(int, SendRequest, (uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option), (override));
+//     MOCK_METHOD(bool, AddDeathRecipient, (const sptr<DeathRecipient> &recipient), (override));
+//     MOCK_METHOD(bool, RemoveDeathRecipient, (const sptr<DeathRecipient> &recipient), (override));
+//     MOCK_METHOD(int, Dump, (int fd, const std::vector<std::u16string> &args), (override));
+// };
+
+class TestMidiCallbackStub : public IRemoteStub<IMidiCallback> {
+public:
+    int32_t NotifyDeviceChange(int32_t, const std::map<int32_t, std::string>&) override { return 0; }
+    int32_t NotifyError(int32_t) override { return 0; }
+};
+
+class MockMidiServiceController: public MidiServiceController {
+    MOCK_METHOD(int32_t, CreateClientInServer, (std::shared_ptr<MidiServiceCallback> callback, sptr<IRemoteObject> &client,
+                                 uint32_t &clientId));
+};
+
+class MidiServerUnitTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+
+    static void TearDownTestCase() {}
+
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
+
+/**
+ * @tc.name: MidiClientInServer_GetDevices001
+ * @tc.desc: call controller's GetDevices(), expect OK
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_GetDevices001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    uint32_t id = 123;
+    std::vector<std::map<int32_t, std::string>> devices;
+
+    MidiClientInServer client(id, mockCallback);
+    EXPECT_EQ(MIDI_STATUS_OK, client.GetDevices(devices));
+    EXPECT_TRUE(devices.empty());
+}
+
+/**
+ * @tc.name: MidiClientInServer_GetDevicePorts001
+ * @tc.desc: call controller's GetDevicePorts(), expect OK
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_GetDevicePorts001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    uint32_t id = 123;
+    int64_t deviceId = 12345;
+    std::vector<std::map<int32_t, std::string>> ports;
+
+    MidiClientInServer client(id, mockCallback);
+    EXPECT_EQ(MIDI_STATUS_OK, client.GetDevicePorts(deviceId, ports));
+    EXPECT_TRUE(ports.empty());
+}
+
+
+/**
+ * @tc.name: MidiClientInServer_OpenDevice001
+ * @tc.desc: call controller's OpenDevice()
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_OpenDevice001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    uint32_t id = 123;
+    int64_t deviceId = 12345;
+
+    MidiClientInServer client(id, mockCallback);
+    EXPECT_NE(MIDI_STATUS_OK, client.OpenDevice(deviceId));
+}
+
+/**
+ * @tc.name: MidiClientInServer_OpenInputPort001
+ * @tc.desc: call controller's OpenInputPort()
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_OpenInputPort001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    uint32_t id = 123;
+    std::shared_ptr<SharedMidiRing> buffer;
+    int64_t deviceId = 12345;
+    uint32_t portIndex = 1;
+
+
+    MidiClientInServer client(id, mockCallback);
+    EXPECT_NE(MIDI_STATUS_OK, client.OpenInputPort(buffer, deviceId, portIndex));
+}
+
+/**
+ * @tc.name: MidiClientInServer_CloseInputPort001
+ * @tc.desc: call controller's CloseInputPort()
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_CloseInputPort001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    uint32_t id = 123;
+    int64_t deviceId = 12345;
+    uint32_t portIndex = 1;
+    MidiClientInServer client(id, mockCallback);
+    EXPECT_NE(MIDI_STATUS_OK, client.CloseInputPort(deviceId, portIndex));
+}
+
+/**
+ * @tc.name: MidiClientInServer_CloseDevice001
+ * @tc.desc: call controller's CloseDevice()
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_CloseDevice001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    uint32_t id = 123;
+    int64_t deviceId = 12345;
+
+    MidiClientInServer client(id, mockCallback);
+    EXPECT_NE(MIDI_STATUS_OK, client.CloseDevice(deviceId));
+}
+
+/**
+ * @tc.name: MidiClientInServer_DestroyMidiClient001
+ * @tc.desc: call controller's DestroyMidiClient()
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_DestroyMidiClient001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    uint32_t id = 123;
+
+    MidiClientInServer client(id, mockCallback);
+    EXPECT_NE(MIDI_STATUS_OK, client.DestroyMidiClient());
+}
+
+/**
+ * @tc.name: MidiClientInServer_NotifyError001
+ * @tc.desc: call callback's NotifyError
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiClientInServer_NotifyError001, TestSize.Level0)
+{
+    auto mockCallback = std::make_shared<MockMidiServiceCallback>();
+    auto rawCallback = mockCallback.get();
+    uint32_t id = 123;
+    EXPECT_CALL(*rawCallback, NotifyError(-1)).Times(1);
+
+    MidiClientInServer client(id, mockCallback);
+    client.NotifyError(-1);
+    ASSERT_NE(nullptr, client.callback_);
+}
+
+/**
+ * @tc.name: MidiListenerCallback_NotifyDeviceChange001
+ * @tc.desc: call callback's NotifyDeviceChange
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiListenerCallback_NotifyDeviceChange001, TestSize.Level0)
+{
+    std::map<int32_t, std::string> devicdInfo = {{1, "piano"}};
+    sptr<MockIMidiCallback> mockCallback = sptr<MockIMidiCallback>::MakeSptr();
+    EXPECT_CALL(*mockCallback, NotifyDeviceChange(ADD, devicdInfo)).Times(1);
+
+    MidiListenerCallback listener(mockCallback);
+    listener.NotifyDeviceChange(ADD, devicdInfo);
+    EXPECT_NE(nullptr, listener.callback_);
+}
+
+/**
+ * @tc.name: MidiListenerCallback_NotifyError001
+ * @tc.desc: call callback's NotifyError
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiListenerCallback_NotifyError001, TestSize.Level0)
+{
+    sptr<MockIMidiCallback> mockCallback = sptr<MockIMidiCallback>::MakeSptr();
+    EXPECT_CALL(*mockCallback, NotifyError(-1)).Times(1);
+
+    MidiListenerCallback listener(mockCallback);
+    listener.NotifyError(-1);
+    EXPECT_NE(nullptr, listener.callback_);
+}
+
+
+/**
+ * @tc.name: MidiServer_OnStart001
+ * @tc.desc: call callback's OnStart
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiServer_OnStart001, TestSize.Level0)
+{
+    int32_t systemAbilityId = 123;
+    sptr<MidiServer> server = sptr<MidiServer>::MakeSptr(systemAbilityId, true);
+    ASSERT_NE(nullptr, server);
+    server->OnStart();
+    server->OnDump();
+    EXPECT_NE(nullptr, server->controller_);
+}
+
+/**
+ * @tc.name: MidiServer_CreateClientInServer001
+ * @tc.desc: call callback's CreateClientInServer
+ * @tc.type: FUNC
+ */
+
+HWTEST_F(MidiServerUnitTest, MidiServer_CreateClientInServer001, TestSize.Level0)
+{
+    int32_t systemAbilityId = 123;
+    sptr<MidiServer> server = sptr<MidiServer>::MakeSptr(systemAbilityId, true);
+    MockMidiServiceController *controler = new MockMidiServiceController();
+    sptr<IRemoteObject> object = (new TestMidiCallbackStub())->AsObject();
+    sptr<IRemoteObject> client;
+    uint32_t clientId = 0;
+    server->controller_ = controler;
+    ASSERT_NE(server->controller_, nullptr);
+    ASSERT_NE(object, nullptr);
+
+    // EXPECT_CALL(*controler, CreateClientInServer(_, _, _))
+    //     .WillOnce(Return(MIDI_STATUS_OK));
+    
+    EXPECT_NE(nullptr, server->controller_);
+    EXPECT_EQ(MIDI_STATUS_OK, server->CreateClientInServer(object, client, clientId));
+    delete controler;
+}
